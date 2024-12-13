596b92058a6390827e00a7524bac7af191d977cc
Gabriel Hernandez <gabhernandez@hotmail.ca>
2024-12-13 11:39:27 -0500

    function to add product to the inventory has been created

--- Store.js
+++ Store.js
@@ -1,5 +1,12 @@
+//Store class
 class Store {
     constructor() {
         this.inventory = [];
     }
+
+// Add a product to the inventory
+addProduct(product) {
+    this.inventory.push(product);
+ }
+ 
 }
