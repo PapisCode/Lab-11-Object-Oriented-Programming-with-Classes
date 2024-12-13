ddbf924beaa9db4c36e5e209b93232271dc3530e
Gabriel Hernandez <gabhernandez@hotmail.ca>
2024-12-13 13:59:04 -0500

    all errors that were showing up under the Product.js file are nor finalized, ran my node main.js in terminal and program is running smoothly.

--- Product.js
+++ Product.js
@@ -10,16 +10,16 @@ class Product {
 getTotalValue() {
     return this.price * this.quantity;
  }
- 
+
  // String representation of the product
  toString() {
-    return `Product ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
  }
 
  // Method to apply the discount to any array of products
  static applyDiscount(products, discount) {
-    products.forEach(products => {
-        products.price -= product.price * discount;
+    products.forEach(product => {
+        product.price -= product.price * discount;
         });
     }
 }
