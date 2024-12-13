ffb8e0233589cf57b3469ecd3301e9ad1c658142
Gabriel Hernandez <gabhernandez@hotmail.ca>
2024-12-13 13:21:19 -0500

    added 5 products as per lab rules

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const Product = require ('./Product');
 const PerishableProduct = require ('./PerishableProduct');
 const Store = require ('./Store');
+
+//Create Products
+const product1 = new Product('Laptop', 800, 5);
+const product2 = new Product('Monitor', 450, 5);
+const product3 = new Product('Mouse', 100, 10);
+const product4 = new Product('Storage Device', 85, 10);
+const product5 = new Product('Chair', 500, 6);
