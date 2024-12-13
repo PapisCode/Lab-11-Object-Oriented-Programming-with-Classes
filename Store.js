70c4188f50061be9707822e33854b9ac7e0ce92b
Gabriel Hernandez <gabhernandez@hotmail.ca>
2024-12-13 11:49:15 -0500

    function to remove an item from the inventory has been created (optional feature created for more practice)

--- Store.js
+++ Store.js
@@ -7,6 +7,15 @@ class Store {
 // Add a product to the inventory
 addProduct(product) {
     this.inventory.push(product);
- }
- 
+}
+
+// Remove a product from the inventory
+removeProduct(product) {
+    const index = this.inventory.indexOf(product);
+    if (index === -1) {
+        console.log('Product not found in inventory.');
+    } else {
+        this.inventory.splice(index, 1);
+        console.log(`Product "${product.name}" has been removed from the inventory`);
+    }
 }
